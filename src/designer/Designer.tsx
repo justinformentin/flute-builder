ca14f37837bc88ed13f681d9e15ac23bfe4f976c
Justin Formentin <justin.formentin@gmail.com>
2026-08-09 23:16:00 -0400

    Correct tone hole size calculation

--- src/designer/Designer.tsx
+++ src/designer/Designer.tsx
@@ -353,7 +353,7 @@ export function Designer() {
           <span>SCALED CONSTRUCTION VIEW</span>
           <span className="text-teal">CENTER MEASUREMENTS</span>
         </div>
-        <FluteDiagram result={result} labels={noteLabels} />
+        <FluteDiagram result={result} labels={noteLabels} outsideDiameterMm={design.outsideDiameterMm} />
         <DesignResults
           design={design}
           result={result}
